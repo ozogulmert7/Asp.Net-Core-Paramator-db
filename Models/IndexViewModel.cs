9182b47ad01a5de85821c6fc5ed873edf3131915
MertJokjeck <ozogulmert7@gmail.com>
2023-05-18 15:25:09 +0300

    change

--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -7,9 +7,7 @@ public class IndexViewModel
     public IEnumerable<Site>? Sites { get; set; }
     public Site? Site { get; set; }
     public IEnumerable<Slide>? Slides { get; set; }
-    public Slide? Slide { get; set; }
     public IEnumerable<About>? Abouts { get; set; }
     public About? About { get; set; }
     public IEnumerable<Team>? Teams { get; set; }
-    public Team? Team { get; set; }
 }
